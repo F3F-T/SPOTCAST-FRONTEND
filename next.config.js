c86e79b5ef492c9bed445c42250347f3a5ef7f52
lazyreport <kinghyejoo@gmail.com>
2022-12-14 17:31:16 +0900

    update to Next.js 13 (beta)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+
 }
 
 module.exports = nextConfig
