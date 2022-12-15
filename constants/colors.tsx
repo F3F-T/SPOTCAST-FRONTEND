9929da4b95be8ebb5cc78ce74b043ff4f5048e55
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-16 00:35:34 +0900

    chore: 색상 상수화

---
+++ constants/colors.tsx
@@ -0,0 +1,28 @@
+export const WHITE = "#FFF";
+export const BLACK = "#000";
+export const GREY = {
+  100: "#F1F5F5",
+  200: "#EAEEEF",
+  300: "#E1E4E6",
+  400: "#CED3D6",
+  500: "#A9AFB3",
+  600: "#878D91",
+  700: "#4D5256",
+  800: "#363A3C",
+  900: "#121212",
+};
+
+export const RED = {
+  LIGHT: "#FF8E89",
+  DARK: "#F24147",
+};
+
+export const GREEN = {
+  LIGHT: "#76E8AD",
+  DARK: "#1BBF83",
+};
+
+export const MUSTARD = "#FEC107";
+export const INDIGO = "#020088";
+// export const MUSTARD_MUTED: "#";
+// export const INDIGO_MUTED: "#";
