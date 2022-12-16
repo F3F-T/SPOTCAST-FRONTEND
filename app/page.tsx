b815b64a2872736392aaf259f998760f99d43df4
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-16 21:30:29 +0900

    chore: common 폴더 생성

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styled from "@emotion/styled";
-import Button from "../src/components/button";
+import Button from "../src/components/common/button";
 
 const BTN_TYPE1 = styled(Button)``;
 const BTN_TYPE2 = styled(Button)``;
@@ -14,17 +14,11 @@ export default function Page() {
       Hello, Next.js!
       <BTN_TYPE1
         title="더 많은 영감 받으러가기 > "
-        className="type1"
         type="primary"
         size="large"
       />
-      <BTN_TYPE2 title="전체보기" className="type2" type="text" size="medium" />
-      <BTN_TYPE3
-        title="내 포트폴리오 등록"
-        className="type3"
-        type="secondary"
-        size="small"
-      />
+      <BTN_TYPE2 title="전체보기" type="text" size="medium" />
+      <BTN_TYPE3 title="내 포트폴리오 등록" type="secondary" size="small" />
     </h1>
   );
 }
