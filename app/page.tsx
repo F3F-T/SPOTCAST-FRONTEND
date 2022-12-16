53c6fa73db05ca837d7ac51102830cede77db729
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-16 21:35:08 +0900

    design: 색상 추가

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,12 @@ export default function Page() {
         type="primary"
         size="large"
       />
+      <Button title="basic navy" />
       <BTN_TYPE2 title="전체보기" type="text" size="medium" />
       <BTN_TYPE3 title="내 포트폴리오 등록" type="secondary" size="small" />
+      <Button title="basic navy" type="primary" />
+      <Button title="basic mustard" type="secondary" />
+      <Button title="basic text" type="text" />
     </h1>
   );
 }
