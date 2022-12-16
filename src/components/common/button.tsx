4195b68cfda2537d945643783f635968dbb6de97
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-17 03:09:47 +0900

    feat: 너비 동적 스타일링 구현

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,38 +4,41 @@ import React from "react";
 import styled from "@emotion/styled";
 import {
   INDIGO,
-  INDIGO_DARK,
+  INDIGO_MUTED,
   MUSTARD,
-  MUSTARD_DARK,
+  MUSTARD_MUTED,
   GREY,
   WHITE,
 } from "../../constants/colors";
 
 export type ButtonSize = "large" | "medium" | "small";
-export type ButtonType = "primary" | "secondary" | "text";
-
+export type ButtonTheme = "primary" | "secondary" | "tertiary";
 export type ButtonProps = {
   size?: ButtonSize;
-  type?: ButtonType;
+  theme?: ButtonTheme;
   title: string;
+  width?: string;
   disabled?: boolean;
   onClick?: (e?: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  // iconOnly?: boolean;
 };
 
 export default function Button({
   size = "medium",
-  type = "text",
+  theme = "primary",
   title,
-  onClick,
+  width,
   disabled = false,
+  onClick,
 }: ButtonProps) {
   return (
     //   eslint-disable-next-line @typescript-eslint/no-use-before-define
     <Wrapper
       size={size}
-      buttonType={type}
+      ButtonTheme={theme}
       onClick={onClick}
       disabled={disabled}
+      width={width!}
     >
       {title}
     </Wrapper>
@@ -45,37 +48,35 @@ export default function Button({
 // export default React.memo(Button);
 const Wrapper = styled.button<{
   size: ButtonSize;
+  width: string;
   disabled: boolean;
-  buttonType: ButtonType;
+  ButtonTheme: ButtonTheme;
 }>`
-  ${({ size, disabled, buttonType }) => `
-   padding:  ${
-     // eslint-disable-next-line no-nested-ternary
-     size === "large"
-       ? "1.2rem 20rem"
-       : size === "medium"
-       ? "1rem 4rem"
-       : "1rem 2rem"
+  ${({ size, disabled, ButtonTheme, width }) => `
+   width: ${width};
+   height: ${{ large: "3rem", medium: "2.5rem", small: "1.75rem" }[size]};
+   font-size: ${{ large: "1.2rem", medium: "1rem", small: "0.75rem" }[size]};
+   padding: ${
+     { large: "0 1.5rem", medium: "0 1rem", small: "0 0.875rem" }[size]
    };
-  font-size: ${{ large: "2rem", medium: "1,7rem", small: "1.2rem" }[size]};
   cursor: ${disabled ? "default" : "pointer"};
-  color: ${buttonType === "text" ? GREY[500] : WHITE};
+  color: ${ButtonTheme === "tertiary" ? GREY[500] : WHITE};
   background-color: ${
-    { primary: INDIGO, secondary: MUSTARD, text: WHITE }[buttonType]
+    { primary: INDIGO, secondary: MUSTARD, tertiary: WHITE }[ButtonTheme]
   };
   ${
     !disabled &&
     `&:hover {
       background-color: ${
-        { primary: INDIGO_DARK, secondary: MUSTARD_DARK, text: WHITE }[
-          buttonType
+        { primary: INDIGO_MUTED, secondary: MUSTARD_MUTED, tertiary: WHITE }[
+          ButtonTheme
         ]
       };
-      border-color: ${buttonType === "text" ? GREY[600] : null};
-      color:  ${buttonType === "text" ? GREY[600] : null};
+      border-color: ${ButtonTheme === "tertiary" ? GREY[600] : null};
+      color:  ${ButtonTheme === "tertiary" ? GREY[600] : null};
     }`
   }
-  border: ${buttonType !== "text" ? "none" : `1px solid ${GREY[500]}`};
+  border: ${ButtonTheme !== "tertiary" ? "none" : `1px solid ${GREY[500]}`};
 `}
   display: flex;
   justify-content: center;
