e7c2957386b523a47e08f15475baf4ed6d4c83aa
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-21 02:34:03 +0900

    feat: 메인 아티클1 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from "react";
-import Footer from "../src/components/footer";
-import Carousel from "../src/components/carousel";
+import Footer from "../src/components/main/footer";
+import Carousel from "../src/components/main/carousel";
+import MainArticle1 from "../src/components/main/section/main-article1";
 
 export default function Page() {
   return (
     <>
       <Carousel />
+      <MainArticle1 />
       <Footer />
     </>
   );
