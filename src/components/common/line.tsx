2799750b60b5b3d96879b7f4379ac5997f912b30
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-20 16:04:20 +0900

    refactor: LineProps 수정

--- src/components/common/line.tsx
+++ src/components/common/line.tsx
@@ -4,14 +4,11 @@ import styled from "@emotion/styled";
 export type LineProps = {
   // 선 색상
   color: string;
-  //   선 너비
+  // 선 너비
   width: string;
 };
-export default function Line({ color, width }: LineProps) {
-  return <StyledLine color={color} width={width} />;
-}
 
-export const StyledLine = styled.div`
+export const StyledLine = styled.div<{ color: string; width: string }>`
   ${({ color, width }) => `
     border-bottom: 0.1rem solid ${color};
     width: ${width};
@@ -19,3 +16,7 @@ export const StyledLine = styled.div`
   margin: 0 auto;
   display: block;
 `;
+
+export default function Line({ color, width }: LineProps) {
+  return <StyledLine color={color} width={width} />;
+}
