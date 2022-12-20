d92246c1807588c522f9a8561793aa44b8bb1817
lazyreport <kinghyejoo@gmail.com>
2022-12-21 04:54:36 +0900

    feat. FlexContainer 컴포넌트 생성

---
+++ src/components/common/FlexContainer.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+
+type DirectionType = "row" | "column";
+type PositionType = "flex-start" | "center" | "flex-end";
+
+interface FlexProps {
+  children: React.ReactNode;
+  direction: DirectionType;
+  gap: string;
+  align?: PositionType;
+  justify?: PositionType;
+  width?: string;
+}
+
+const FlexContainer = ({
+  children,
+  direction,
+  gap,
+  align = "flex-start",
+  justify = "flex-start",
+  width = "100%",
+}: FlexProps) => {
+  const Wrapper = styled.div<{
+    children: React.ReactNode;
+    direction: DirectionType;
+    gap: string;
+    align?: PositionType;
+    justify?: PositionType;
+    width?: string;
+  }>`
+    display: flex;
+    ${() => `
+        flex-direction: ${direction};
+        gap: ${gap};
+        width: ${width};
+        align-items: ${align};
+        justify-content: ${justify}
+    `}
+  `;
+  return (
+    <Wrapper
+      direction={direction}
+      gap={gap}
+      align={align}
+      justify={justify}
+      width={width}
+    >
+      {children}
+    </Wrapper>
+  );
+};
+
+export default FlexContainer;
