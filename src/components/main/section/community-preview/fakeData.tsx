bda5408930062a6b163acff5803bdd3435c3bf12
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-21 23:03:32 +0900

    feat: post 더미데이터 추가

---
+++ src/components/main/section/community-preview/fakeData.tsx
@@ -0,0 +1,69 @@
+interface PostProps {
+  id: string;
+  time: number;
+  views: number;
+  comments: number;
+  content: string;
+}
+
+const posts: PostProps[] = [
+  {
+    id: "dbscogus4467",
+    time: 10,
+    views: 873,
+    comments: 56,
+    content: `연기 스터디멤버 구합니다:)`,
+  },
+  {
+    id: "dfsdkjg",
+    time: 3,
+    views: 3435,
+    comments: 98,
+    content: "졸업작품 모델 해주실 분 계실까요(페이 있음)",
+  },
+  {
+    id: "도파민 중독",
+    time: 27,
+    views: 53,
+    comments: 5,
+    content: "진로 고민이 있습니다.",
+  },
+  {
+    id: "erweu123",
+    time: 9,
+    views: 234,
+    comments: 35,
+    content: "영상음악가들은 어떤 경로로 일하기 시작하나요?",
+  },
+  {
+    id: "12132",
+    time: 1,
+    views: 3,
+    comments: 0,
+    content: "영화 미술팀에 가고 싶은데 나이와 학력을 보나요 ?",
+  },
+  {
+    id: "dfsdkjg",
+    time: 1,
+    views: 14,
+    comments: 0,
+    content: "지하철역에서 촬영해보신 분들 조언 구합니다.",
+  },
+  {
+    id: "dudghk76",
+    time: 1,
+    views: 10,
+    comments: 2,
+    content: "문화콘텐츠학과에 가는게 영화계로 가는데 도움이 될까요?",
+  },
+  {
+    id: "dbscogus4467",
+    time: 2,
+    views: 5,
+    comments: 3,
+    content:
+      "이 뮤직비디오의 노래 제목 혹은 가수 이름을 아시는 분 계실까요 ㅠㅠ??",
+  },
+];
+
+export default posts;
