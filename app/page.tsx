dc2aa1470c9692dde0cd5c494d752bfb3d0a5077
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-24 02:50:06 +0900

    feat: 헤더 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import React from "react";
-import Footer from "../src/components/main/footer";
-import Carousel from "../src/components/main/carousel";
-import MainArticle1 from "../src/components/main/section/hot-potfolio";
-import MainArticle3 from "../src/components/main/section/community-preview";
+import Footer from "../src/components/main/Footer";
+import Carousel from "../src/components/main/Carousel";
+import MainArticle1 from "../src/components/main/section/Hot-Potfolio";
+import MainArticle3 from "../src/components/main/section/Community-Preview";
+import Header from "../src/components/main/Header";
 
 export default function Page() {
   return (
     <>
+      <Header />
       <Carousel />
       <MainArticle1 />
       <MainArticle3 />
