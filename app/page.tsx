1770865b3c5234f707040b7015898d7dfde7dd69
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-24 02:57:08 +0900

    style: 헤더 루트레이아웃에 적용

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ import Footer from "../src/components/main/Footer";
 import Carousel from "../src/components/main/Carousel";
 import MainArticle1 from "../src/components/main/section/Hot-Potfolio";
 import MainArticle3 from "../src/components/main/section/Community-Preview";
-import Header from "../src/components/main/Header";
 
 export default function Page() {
   return (
     <>
-      <Header />
       <Carousel />
       <MainArticle1 />
       <MainArticle3 />
