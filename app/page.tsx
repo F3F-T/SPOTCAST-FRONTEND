5b461b3ef6b5da42f839b1bd8d72df0e7f0de6d8
lazyreport <kinghyejoo@gmail.com>
2022-12-26 18:03:04 +0900

    chore. app/page.tsx 임포트 루트 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 import React from "react";
 import Footer from "../src/components/main/Footer";
 import Carousel from "../src/components/main/Carousel";
-import MainArticle1 from "../src/components/main/section/hot-potfolio";
-import MainArticle3 from "../src/components/main/section/community-preview";
-import MainArticle2 from "../src/components/main/section/find-crew";
+import MainArticle1 from "../src/components/main/section/hotPortfolio";
+import MainArticle3 from "../src/components/main/section/communityPreview";
+import MainArticle2 from "../src/components/main/section/findCrew";
 
 export default function Page() {
   return (
