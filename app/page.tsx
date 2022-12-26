11bcb1c5a46eac40231cc6848c92fa5ecb770a5a
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-26 16:25:26 +0900

    design: css 상세 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Footer from "../src/components/main/Footer";
+import Header from "../src/components/main/Header";
 import Carousel from "../src/components/main/Carousel";
 import MainArticle1 from "../src/components/main/section/Hot-Potfolio";
 import MainArticle3 from "../src/components/main/section/Community-Preview";
@@ -9,6 +10,7 @@ import MainArticle3 from "../src/components/main/section/Community-Preview";
 export default function Page() {
   return (
     <>
+      <Header />
       <Carousel />
       <MainArticle1 />
       <MainArticle3 />
