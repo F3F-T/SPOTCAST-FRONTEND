ebb00a6c12f697b630ac53bd2e385e0d16c2e52c
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-29 01:32:35 +0900

    feat: loadUser 임시 구현

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
 import Input from "../common/Input";
 import useInput from "../../hooks/useInput";
 import {
@@ -19,17 +20,20 @@ import {
   SNSIMG,
   SignUp,
 } from "./index.styles";
+import { loadUser } from "../../../stores/reducers/user";
 import { login } from "../../hooks/useAuth";
 
 export default function Login() {
   const router = useRouter();
   const [email, onChangeEmail] = useInput("");
   const [password, onChangePassword] = useInput("");
+  const dispatch = useDispatch();
 
   const onSubmitForm = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       login(email, password);
+      dispatch(loadUser({ email, password }));
       router.replace("/");
     },
     [email, password],
