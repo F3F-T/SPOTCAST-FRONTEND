e2643413f44d93f0aa6e891c10a03b4717fdc6d7
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-12-28 21:02:33 +0900

    refactor: api config 수정

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -2,6 +2,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 // const API_VER = ""; => API 미정이라 주석
 const ME = "/member/"; // 접속한 유저
 const AUTH = "/auth/";
+const LOGIN = "/auth/login/";
+const SIGNUP = "/auth/signup/";
 // const GOOGLE = "/auth/google/"; => API 미정이라 주석
 // const KAKAO = "/auth/kakao/"; => API 미정이라 주석
 const USER = "/user/"; // 다른 유저
@@ -9,11 +11,14 @@ const BOARD = "/board/";
 const PORTFFOLIO = "/portfolio/";
 
 const API = {
-  ME: `${BASE_URL}${ME}`,
-  AUTH: `${BASE_URL}${AUTH}`,
-  USER: `${BASE_URL}${USER}`,
-  BOARD: `${BASE_URL}${BOARD}`,
-  PORTFOLIO: `${BASE_URL}${PORTFFOLIO}`,
+  BASE_URL: `${BASE_URL}`,
+  ME: `${ME}`,
+  AUTH: `${AUTH}`,
+  LOGIN: `${LOGIN}`,
+  SIGNUP: `${SIGNUP}`,
+  USER: `${USER}`,
+  BOARD: `${BOARD}`,
+  PORTFOLIO: `${PORTFFOLIO}`,
 };
 
 export default API;
