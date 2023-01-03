bebb5ca1ab08ce52636604cb3c0c70a39bea44fc
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-03 20:24:27 +0900

    feat: 이메일 유효성 검사 구현

--- src/hooks/useSignUp.tsx
+++ src/hooks/useSignUp.tsx
@@ -1,35 +1,51 @@
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { useCallback } from "react";
-import { authSignUp } from "../api/auth";
+import { authSignUp, authEmail } from "../api/auth";
 import { AppDispatch } from "../../stores/store/configureStore";
 import useInput from "./useInput";
 
 export default function useSignUp() {
   const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
 
   const [email, onChangeEmail] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [passwordCheck, onChangePasswordCheck] = useInput("");
 
-  const dispatch = useDispatch<AppDispatch>();
+  const emailReg =
+    /^[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z가-힣])*@[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const isValidEmail = emailReg.test(email);
 
-  const onSubmitForm = useCallback(
-    (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      dispatch(authSignUp({ email, password }));
-      router.push("/");
-    },
-    [email, password, passwordCheck],
-  );
+  const onCheckEmailAuth = () => {
+    authEmail(email);
+  };
+
+  const onReplaceBack = () => {
+    router.push("/signup");
+  };
+
+  const onReplaceNext = () => {
+    router.push("/signup/email/inform");
+  };
+
+  const onSubmitForm = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(authSignUp({ email, password }));
+    router.push("/");
+  }, []);
 
   return {
     email,
+    isValidEmail,
     password,
     passwordCheck,
     onChangeEmail,
     onChangePassword,
     onChangePasswordCheck,
+    onCheckEmailAuth,
+    onReplaceBack,
+    onReplaceNext,
     onSubmitForm,
   };
 }
