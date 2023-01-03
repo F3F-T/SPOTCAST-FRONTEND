8c839b53055ce29d2e9484bbee384ad44c969872
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-04 00:21:46 +0900

    feat: 이메일 인증 타이머 구현

--- src/hooks/useSignUp.tsx
+++ src/hooks/useSignUp.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
-import { useCallback } from "react";
-import { authSignUp, authEmail } from "../api/auth";
+import { useCallback, useState, useRef, useEffect } from "react";
+import { authSignUp, authEmailSend, authEmailConfirms } from "../api/auth";
 import { AppDispatch } from "../../stores/store/configureStore";
 import useInput from "./useInput";
 
@@ -10,15 +10,59 @@ export default function useSignUp() {
   const dispatch = useDispatch<AppDispatch>();
 
   const [email, onChangeEmail] = useInput("");
+  const [authNumber, onChangeAuthNumber] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [passwordCheck, onChangePasswordCheck] = useInput("");
+  const [isEmailConfirms, setIsEmailConfirms] = useState(false);
 
   const emailReg =
     /^[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z가-힣])*@[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   const isValidEmail = emailReg.test(email);
 
   const onCheckEmailAuth = () => {
-    authEmail(email);
+    authEmailSend(email);
+  };
+
+  const onSubmitEmailAuth = useCallback(() => {
+    authEmailConfirms(email, authNumber)
+      .then(res => {
+        const isConfirmedEmail = res.data;
+        if (isConfirmedEmail) setIsEmailConfirms(true);
+      })
+      .catch(err => {
+        if (err.response.status === 403) {
+          console.error("에러!");
+        }
+      });
+  }, [authNumber]);
+
+  const AuthTimer = () => {
+    const VALIDTIME = 60;
+    const time = useRef<number>(VALIDTIME);
+
+    const intervalRef: { current: NodeJS.Timeout | null } = useRef(null);
+
+    const [min, setMin] = useState(1);
+    const [sec, setSec] = useState(0);
+
+    const decreaseNum = () => {
+      time.current -= 1;
+      setMin(Math.floor(time.current / 60));
+      setSec(time.current % 60);
+    };
+    const onStartTimer = () => {
+      onCheckEmailAuth();
+      intervalRef.current = setInterval(decreaseNum, 1000);
+      return () => clearInterval(intervalRef.current as NodeJS.Timeout);
+    };
+
+    useEffect(() => {
+      if (time.current <= 0) {
+        clearInterval(intervalRef.current as NodeJS.Timeout);
+      }
+    }, [sec]);
+
+    return { time, min, sec, onStartTimer };
   };
 
   const onReplaceBack = () => {
@@ -38,14 +82,19 @@ export default function useSignUp() {
   return {
     email,
     isValidEmail,
+    authNumber,
+    onChangeAuthNumber,
     password,
     passwordCheck,
     onChangeEmail,
     onChangePassword,
     onChangePasswordCheck,
     onCheckEmailAuth,
+    isEmailConfirms,
     onReplaceBack,
     onReplaceNext,
     onSubmitForm,
+    onSubmitEmailAuth,
+    AuthTimer,
   };
 }
