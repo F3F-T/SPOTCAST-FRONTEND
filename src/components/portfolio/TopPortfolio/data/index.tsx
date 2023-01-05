872aa949bcbaff576100373517b50e23e64cc572
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-05 18:23:53 +0900

    chore: 더미 데이터 추가

---
+++ src/components/portfolio/TopPortfolio/data/index.tsx
@@ -0,0 +1,12 @@
+const list = [
+  { name: "임채환", role: "드러머 • 프리랜서", src: ["/images/img2.png"] },
+  { name: "박유빈", role: "포토그래퍼", src: ["/images/img3.png"] },
+
+  {
+    name: "임민정",
+    role: "모델 • 프리랜서",
+    src: ["/images/img1-1.png", "/images/img1-2.png"],
+  },
+];
+
+export default list;
