afc7347fcad29566e49385ac6eb491e1b17089c2
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-10 18:34:44 +0900

    refactor: api config 수정

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -2,20 +2,24 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 // const API_VER = ""; => API 미정이라 주석
 const ME = "/member/"; // 접속한 유저
 const AUTH = "/auth/";
-const LOGIN = "/auth/login/";
-const SIGNUP = "/auth/signup/";
-// const GOOGLE = "/auth/google/"; => API 미정이라 주석
-// const KAKAO = "/auth/kakao/"; => API 미정이라 주석
 const USER = "/user/"; // 다른 유저
 const BOARD = "/board/";
 const PORTFFOLIO = "/portfolio/";
+const LOGIN = "login";
+const SIGNUP = "signup";
+const EMAIL_CERTIFICATION_SEND = "email-certification/sends";
+const EMAIL_CERTIFICATION_CONFIRM = "email-certification/confirms";
+// const GOOGLE = "/auth/google/"; => API 미정이라 주석
+// const KAKAO = "/auth/kakao/"; => API 미정이라 주석
 
 const API = {
   BASE_URL: `${BASE_URL}`,
-  ME: `${ME}`,
   AUTH: `${AUTH}`,
-  LOGIN: `${LOGIN}`,
-  SIGNUP: `${SIGNUP}`,
+  LOGIN: `${AUTH}${LOGIN}`,
+  EMAIL_SEND: `${AUTH}${EMAIL_CERTIFICATION_SEND}`,
+  EMAIL_CONFIRM: `${AUTH}${EMAIL_CERTIFICATION_CONFIRM}`,
+  SIGNUP: `${AUTH}${SIGNUP}`,
+  ME: `${ME}`,
   USER: `${USER}`,
   BOARD: `${BOARD}`,
   PORTFOLIO: `${PORTFFOLIO}`,
