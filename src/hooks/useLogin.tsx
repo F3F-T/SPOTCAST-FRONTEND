6e13b100744b965beb241d1fe134879f858486a8
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-10 22:29:32 +0900

    feat: 로그인 기능 구현 완료

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,10 +1,9 @@
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-import React, { useCallback } from "react";
-import { RootState } from "../../stores/reducers";
+import React, { useCallback, useEffect } from "react";
 import { AppDispatch } from "../../stores/store/configureStore";
 import { authLogin, getMember } from "../api/auth";
-import { loginTest } from "../../stores/reducers/user";
+import { RootState } from "../../stores/reducers";
 
 import useInput from "./useInput";
 
@@ -15,23 +14,29 @@ export default function useLogin() {
   const [password, onChangePassword] = useInput("");
 
   const dispatch = useDispatch<AppDispatch>();
-  const { loginDone, me } = useSelector((state: RootState) => state.user);
+  const { loginDone, loginError, me } = useSelector(
+    (state: RootState) => state.user,
+  );
 
   const onSubmitForm = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       dispatch(authLogin({ email, password }));
-      if (loginDone) {
-        localStorage.setItem("email", me.email);
-        localStorage.setItem("access_token", me.accessToken);
-        console.log(`${me.email}님 로그인이 완료되었습니다.`);
-        dispatch(getMember());
-        router.push("/");
-      }
-      dispatch(loginTest()); // TEST
     },
     [email, password],
   );
+
+  useEffect(() => {
+    if (loginDone) {
+      localStorage.setItem("email", me.email);
+      localStorage.setItem("access_token", me.accessToken);
+      // dispatch(getMember());
+      router.push("/");
+    }
+    if (loginError) {
+      alert(loginError.message);
+    }
+  }, [loginDone, loginError]);
 
   return {
     email,
