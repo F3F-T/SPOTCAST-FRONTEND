e5485f09e7ad17c63d9c8d630159857d45f3c786
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-12 19:11:13 +0900

    feat: SSR+httpOnly Cookie 로그인 유지 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
 import Footer from "../src/components/main/Footer";
 import Header from "../src/components/main/Header";
 import Carousel from "../src/components/main/Carousel";
@@ -8,13 +7,9 @@ import MainArticle1 from "../src/components/main/section/hotPortfolio";
 import MainArticle3 from "../src/components/main/section/communityPreview";
 import MainArticle2 from "../src/components/main/section/findCrew";
 import wrapper from "../stores/store/configureStore";
-import { loginTest } from "../stores/reducers/user";
-import { authLogin, authLogout } from "../src/api/auth";
+import { getMember, getSocialMember } from "../src/api/auth";
 
 export default function Page() {
-  // dispatch(
-  //   authLogin({ email: "dbscogus4467@naver.com", passwrod: "4467cogus" }),
-  // );
   return (
     <div>
       <Header />
@@ -27,19 +22,22 @@ export default function Page() {
   );
 }
 // SSR (프론트 서버에서 실행)
-export const getServerSideProps = wrapper.getServerSideProps(
-  store =>
-    async ({ req, res, ...etc }) => {
-      const cookie = req ? req.headers.cookie : "";
-      axios.defaults.headers.Cookie = "";
-      // 쿠키가 브라우저에 있는경우만 넣어서 실행
-      // (주의, 아래 조건이 없다면 다른 사람으로 로그인 될 수도 있음)
-      if (req && cookie) {
-        axios.defaults.headers.Cookie = cookie;
-      }
+export const getServerSideProps = wrapper.getServerSideProps(store =>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async ({ req, res, ...etc }) => {
+    const cookie = req ? req.headers.cookie : "";
+    // 프론트 서버에서 쿠키를 초기화
+    axios.defaults.headers.Cookie = "";
+    // 쿠키가 브라우저에 있는경우만 넣어서 실행
+    // (주의, 아래 조건이 없다면 다른 사람으로 로그인 될 수도 있음)
+    if (req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+      await store.dispatch(getMember());
+      await store.dispatch(getSocialMember());
+    }
 
-      return {
-        props: {},
-      };
-    },
+    return {
+      props: {},
+    };
+  },
 );
