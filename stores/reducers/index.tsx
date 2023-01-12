9e836dde881c16d78d411444eed4fcedbb653d33
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-12 16:37:21 +0900

    refactor: root, user reducer 수정

--- stores/reducers/index.tsx
+++ stores/reducers/index.tsx
@@ -2,17 +2,18 @@ import { combineReducers } from "redux";
 import { HYDRATE } from "next-redux-wrapper";
 import userSlice from "./user";
 
+// (이전상태, 액션) => 다음상태
 const rootReducer = (state: any, action: any) => {
-  if (action.type === HYDRATE) {
-    return {
-      ...state,
-      ...action.payload,
-    };
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user: userSlice.reducer,
+      });
+      return combinedReducer(state, action);
+    }
   }
-  return combineReducers({
-    user: userSlice.reducer,
-    // 여기에 추가
-  })(state, action);
 };
 
 export type RootState = ReturnType<typeof rootReducer>;
