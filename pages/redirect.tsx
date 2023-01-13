14aeaef1d5176f38b2ae443ef56214ccd7406278
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-14 02:48:30 +0900

    feat: 리다이렉트 페이지 SSR적용

--- pages/redirect.tsx
+++ pages/redirect.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import axios from "axios";
 import Redirct from "../src/components/login/redirect";
+import wrapper from "../stores/store/configureStore";
+import { storeCookie } from "../stores/reducers/user";
+import { getMember } from "../src/api/auth";
 
 export default function Page() {
   return <Redirct />;
 }
+export const getServerSideProps = wrapper.getServerSideProps(store =>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async ({ req, res, ...etc }) => {
+    const cookie = req ? req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+
+    if (req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+      await store.dispatch(storeCookie(cookie));
+    }
+    await store.dispatch(getMember());
+
+    return {
+      props: {},
+    };
+  },
+);
