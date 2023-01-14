a08f25282af99b000b32cf5cbd87f71b12043d1d
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-14 13:52:25 +0900

    feat: user interface 추가

---
+++ src/interface/user.tsx
@@ -0,0 +1,12 @@
+export interface UserProps {
+  authority: string | null;
+  email: string;
+  id: number;
+  information: string | null;
+  instagram: string | null;
+  loginMemberType: string;
+  loginType: string;
+  name: string;
+  otherSns: string | null;
+  twitter: string | null;
+}
