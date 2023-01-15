a6db40f498dce8ed35125392b2645bb7decc7434
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-15 14:17:08 +0900

    feat: SNS 탭 UI 완성

--- src/components/profile/tab/sns/index.tsx
+++ src/components/profile/tab/sns/index.tsx
@@ -1,13 +1,63 @@
 import React from "react";
 import styled from "@emotion/styled";
+import ImgButton from "../../../common/ImgButton";
+import { GREY } from "../../../../constants/colors";
+import Icon from "../../../common/Icon";
 
 const Container = styled.div`
   font-size: 1.5rem;
   display: flex;
   flex-direction: column;
-  gap: 5rem;
+  gap: 1.3rem;
 `;
 
+const SNSbtn = styled(ImgButton)`
+  border: 0.1rem solid ${GREY[300]};
+  border-radius: 10rem;
+  padding: 1rem 1.3rem;
+  font-size: 1.5rem;
+  color: ${GREY[800]};
+  p {
+    font-weight: 500;
+  }
+  &:hover {
+    p {
+      font-weight: 700;
+    }
+    border: 0.1rem solid ${GREY[600]};
+  }
+`;
+const ButtonWrapper = styled.div`
+  position: relative;
+`;
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 1.3rem;
+  left: 1.5rem;
+`;
 export default function SNS() {
-  return <Container>SNS</Container>;
+  return (
+    <Container>
+      <SNSbtn
+        title="instagram"
+        size={2.4}
+        ImgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+      />
+      <SNSbtn
+        title="twitter"
+        size={2.4}
+        ImgSrc="https://upload.wikimedia.org/wikipedia/commons/f/f2/Logo_Twitter.png"
+      />
+      <ButtonWrapper>
+        <SNSbtn
+          title="other"
+          size={2.4}
+          ImgSrc="https://blog.kakaocdn.net/dn/dAEQye/btqDOkONLE0/5TK0HtrButojrSUVadJRP0/img.jpg"
+        />
+        <IconWrapper>
+          <Icon className="plus" size="2.2rem" border={0.1} />
+        </IconWrapper>
+      </ButtonWrapper>
+    </Container>
+  );
 }
