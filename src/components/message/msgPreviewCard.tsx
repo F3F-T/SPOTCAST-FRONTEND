3d8a1515fb4fcb72784d1a76585a14e2dcbf6744
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-21 23:15:29 +0900

    feat: 메시지룸 컴포넌트 단위 구현

---
+++ src/components/message/msgPreviewCard.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { GREY } from "../../constants/colors";
+import { PreviewType } from "./fakeData";
+
+const Preview = styled.div<{
+  selected: boolean;
+}>`
+  ${({ selected }) => `
+    background-color: ${selected ? GREY[200] : "transparent"};
+`}
+  width: 30rem;
+  height: 12rem;
+  border-bottom: 0.1rem solid ${GREY[300]};
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Img = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 10rem;
+  background-color: ${GREY[400]};
+`;
+
+const Name = styled.div`
+  width: 16rem;
+  position: relative;
+  font-size: 1.5rem;
+`;
+const Date = styled.span`
+  position: absolute;
+  right: 0;
+  font-size: 1.2rem;
+  color: ${GREY[500]};
+`;
+const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const Content = styled.div`
+  width: 16rem;
+  height: 3rem;
+  font-size: 1.3rem;
+  line-height: 1.2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: ${GREY[600]};
+`;
+
+export default function MsgPreviewCard({
+  item,
+  selected = false,
+}: PreviewType) {
+  return (
+    <Preview selected={selected}>
+      <Img />
+      <Right>
+        <Name>
+          {item.name} <Date>{item.date}</Date>
+        </Name>
+        <Content>{item.content}</Content>
+      </Right>
+    </Preview>
+  );
+}
