a9075221afbb0f14f0b97886529fc2ba5481d698
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-24 23:16:03 +0900

    feat: context 상태관리 리듀서 추가

--- stores/reducers/message.tsx
+++ stores/reducers/message.tsx
@@ -1,22 +1,38 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from "@reduxjs/toolkit";
-import { loadMsgReceived, loadMsgSend } from "../../src/api/message";
+import {
+  loadMsgReceived,
+  loadMsgSend,
+  sendMessage,
+} from "../../src/api/message";
 
 type MessageState = {
   msgReceived: object[];
   msgSend: object[];
-  loadMsgLoding: boolean;
-  loadMsgDone: boolean;
-  loadMsgError: any;
+  loadMsgSendLoding: boolean;
+  loadMsgSendDone: boolean;
+  loadMsgSendError: any;
+  loadMsgReceivedLoding: boolean;
+  loadMsgReceivedDone: boolean;
+  loadMsgReceivedError: any;
+  sendMsgLoading: boolean;
+  sendMsgDone: boolean;
+  sendMsgError: any;
 };
 
 // 기본 state
 export const initialState: MessageState = {
   msgReceived: [],
   msgSend: [],
-  loadMsgLoding: false,
-  loadMsgDone: false,
-  loadMsgError: null,
+  loadMsgSendLoding: false,
+  loadMsgSendDone: false,
+  loadMsgSendError: null,
+  loadMsgReceivedLoding: false,
+  loadMsgReceivedDone: false,
+  loadMsgReceivedError: null,
+  sendMsgLoading: false,
+  sendMsgDone: false,
+  sendMsgError: null,
 };
 
 const messageSlice = createSlice({
@@ -27,35 +43,48 @@ const messageSlice = createSlice({
     builder
       // 받은 메시지들 가져오기
       .addCase(loadMsgReceived.pending, state => {
-        state.loadMsgLoding = true;
-        state.loadMsgDone = false;
-        state.loadMsgError = null;
+        state.loadMsgReceivedLoding = true;
+        state.loadMsgReceivedDone = false;
+        state.loadMsgReceivedError = null;
       })
       .addCase(loadMsgReceived.fulfilled, (state, action) => {
-        state.loadMsgLoding = false;
+        state.loadMsgReceivedLoding = false;
         state.msgReceived = action.payload;
-        state.loadMsgDone = true;
+        state.loadMsgReceivedDone = true;
       })
       .addCase(loadMsgReceived.rejected, (state, action) => {
-        state.loadMsgLoding = false;
-        state.loadMsgError = action.payload;
+        state.loadMsgReceivedLoding = false;
+        state.loadMsgReceivedError = action.payload;
       })
       // 보낸 메시지들 가져오기
       .addCase(loadMsgSend.pending, state => {
-        state.loadMsgLoding = true;
-        state.loadMsgDone = false;
-        state.loadMsgError = null;
+        state.loadMsgSendLoding = true;
+        state.loadMsgSendDone = false;
+        state.loadMsgSendError = null;
       })
       .addCase(loadMsgSend.fulfilled, (state, action) => {
-        state.loadMsgLoding = false;
+        state.loadMsgSendLoding = false;
         state.msgSend = action.payload;
-        state.loadMsgDone = true;
+        state.loadMsgSendDone = true;
       })
       .addCase(loadMsgSend.rejected, (state, action) => {
-        state.loadMsgLoding = false;
-        state.loadMsgError = action.payload;
+        state.loadMsgSendLoding = false;
+        state.loadMsgSendError = action.payload;
       })
-
+      // 메시지 보내기
+      .addCase(sendMessage.pending, state => {
+        state.sendMsgLoading = true;
+        state.sendMsgDone = false;
+        state.sendMsgError = null;
+      })
+      .addCase(sendMessage.fulfilled, state => {
+        state.sendMsgLoading = false;
+        state.sendMsgDone = true;
+      })
+      .addCase(sendMessage.rejected, (state, action) => {
+        state.sendMsgLoading = false;
+        state.sendMsgError = action.payload;
+      })
       .addDefaultCase(state => state),
 });
 
