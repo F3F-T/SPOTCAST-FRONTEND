a379f38550e26d9b812f43a62ee75bf07639c13a
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-25 03:16:56 +0900

    feat: 메시지 관련 util, interface 추가

--- src/lib/utils/index.tsx
+++ src/lib/utils/index.tsx
@@ -2,7 +2,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../../stores/reducers";
 
-const getUser = () => {
+export const getUser = () => {
   const { IsUserLoggedIn, me } = useSelector((state: RootState) => state.user);
   return {
     IsUserLoggedIn,
@@ -10,4 +10,22 @@ const getUser = () => {
   };
 };
 
-export default getUser;
+export const getMessage = () => {
+  const { msgSend, msgReceived } = useSelector(
+    (state: RootState) => state.message,
+  );
+
+  const msgSendData = {
+    data: msgSend.content,
+    size: msgSend.totalElements,
+  };
+  const msgReceivedData = {
+    data: msgReceived.content,
+    size: msgReceived.totalElements,
+  };
+
+  return {
+    msgSendData,
+    msgReceivedData,
+  };
+};
