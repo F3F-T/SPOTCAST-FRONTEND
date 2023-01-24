91a76dcd9cc9ef51c984c46990d396535eda6948
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-24 23:13:58 +0900

    feat: 다이나믹 라우팅 구현

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import axios from "axios";
-import UserProfile from "../src/components/profile/me";
-import AppLayout from "../styles/AppLayout";
-import wrapper from "../stores/store/configureStore";
-import { storeCookie } from "../stores/reducers/user";
-import { getMember } from "../src/api/auth";
-import { useRedirect } from "../src/hooks/useAuth";
+import UserProfile from "../../src/components/profile/user";
+import AppLayout from "../../styles/AppLayout";
+import wrapper from "../../stores/store/configureStore";
+import { getMember } from "../../src/api/auth";
+import { useRedirect } from "../../src/hooks/useAuth";
 
-function Page() {
+export default function Page() {
   useRedirect();
 
   return (
@@ -17,20 +16,17 @@ function Page() {
   );
 }
 
-export default Page;
-
 export const getServerSideProps = wrapper.getServerSideProps(store =>
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async ({ req, res, ...etc }) => {
+  async ({ req, res, query }) => {
     const cookie = req ? req.headers.cookie : "";
     axios.defaults.headers.Cookie = "";
 
     if (req && cookie) {
       axios.defaults.headers.Cookie = cookie;
-      await store.dispatch(storeCookie(cookie));
     }
     await store.dispatch(getMember());
-
+    // await store.dispatch(getUser({ userId: query.id }));
     return {
       props: {},
     };
---
+++ src/components/profile/user/tab/index.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useCallback } from "react";
+import styled from "@emotion/styled";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../../../stores/reducers";
+import Line from "../../../common/Line";
+import Introduce from "./information";
+import Work from "./work";
+import SNS from "./sns";
+import { GREY } from "../../../../constants/colors";
+
+const Container = styled.div`
+  width: 58.5rem;
+  height: 70rem;
+`;
+
+const Wrapper = styled.div`
+  padding-top: 8.5rem;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  padding-bottom: 1rem;
+  .current {
+    font-weight: 700;
+  }
+  position: relative;
+  align-items: center;
+`;
+
+const TabButton = styled.button`
+  background-color: transparent;
+  border: none;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  &:hover {
+    background-color: ${GREY[100]};
+    border-radius: 5rem;
+  }
+`;
+
+const StyledLine = styled(Line)`
+  margin-bottom: 4rem;
+`;
+
+interface TabTypes {
+  name: string;
+  content: React.ReactNode;
+}
+
+interface TabProps extends Array<TabTypes> {}
+
+export default function Tab() {
+  //   const { IsUserLoggedIn } = useSelector((state: RootState) => state.user);
+  const [currentTab, setCurrentTab] = useState(0);
+  const tabMenu: TabProps = [
+    {
+      name: "소개",
+      content: <Introduce />,
+    },
+    {
+      name: "작업물",
+      content: <Work />,
+    },
+    {
+      name: "SNS",
+      content: <SNS />,
+    },
+  ];
+
+  const tabHandler = useCallback(
+    (index: number) => {
+      setCurrentTab(index);
+    },
+    [tabMenu[currentTab].name],
+  );
+
+  return (
+    <Container>
+      <Wrapper>
+        <ButtonWrapper>
+          {tabMenu.map((item, index) => {
+            return (
+              <TabButton
+                className={index === currentTab ? "current" : ""}
+                onClick={() => tabHandler(index)}
+              >
+                {item.name}
+              </TabButton>
+            );
+          })}
+        </ButtonWrapper>
+        <StyledLine width="100%" />
+        <div>{tabMenu[currentTab].content}</div>
+      </Wrapper>
+    </Container>
+  );
+}
