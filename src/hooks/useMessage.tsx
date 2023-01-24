0b93aaa1373758917d9d917a3146deb8aa119075
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-25 03:14:23 +0900

    refactor: 메시지 관련 함수 hook으로 분리

--- src/hooks/useMessage.tsx
+++ src/hooks/useMessage.tsx
@@ -1,26 +1,25 @@
 import { useDispatch } from "react-redux";
 import { useCallback, useState } from "react";
-import { useRouter } from "next/navigation";
 import { AppDispatch } from "../../stores/store/configureStore";
-import { loadMsgReceived, loadMsgSend, deleteMessage } from "../api/message";
-import { getMessage } from "../util/lib";
+import { loadMsgReceived, loadMsgSend } from "../api/message";
+import { getMessage } from "../lib/utils";
 
 export default function useMessage() {
   const dispatch = useDispatch<AppDispatch>();
-  const router = useRouter();
 
   const MESSAGE_SIZE = 4;
 
   const TabList = ["받은 메시지", "보낸 메시지"];
   const [currentTab, setCurrentTab] = useState("받은 메시지");
-
-  const [currentSendPage, setCurrentSendPage] = useState(1);
-  const [currentReceivedPage, setCurrentReceivedPage] = useState(1);
-
+  const [currentPage, setCurrentPage] = useState(0);
   const { msgSendData, msgReceivedData } = getMessage();
 
-  const msgSendSize = msgSendData.size;
-  const msgReceivedSize = msgReceivedData.size;
+  const msgSendSize = [...Array(Math.ceil(msgSendData.size / 4 || 0))].map(
+    (v, i) => i,
+  );
+  const msgReceivedSize = [
+    ...Array(Math.ceil(msgReceivedData.size / 4 || 0)),
+  ].map((v, i) => i);
 
   const onChangeSendMsg = async (page: number) => {
     await dispatch(loadMsgSend({ page, size: MESSAGE_SIZE }));
@@ -35,40 +34,28 @@ export default function useMessage() {
       setCurrentTab(item);
       if (item === TabList[0]) {
         onChangeReceivedMsg(0);
-        setCurrentReceivedPage(1);
+        setCurrentPage(0);
       }
       if (item === TabList[1]) {
         onChangeSendMsg(0);
-        setCurrentSendPage(1);
+        setCurrentPage(0);
       }
     },
     [currentTab],
   );
-
-  const setSendPage = useCallback(
-    (page: number) => {
-      setCurrentSendPage(page);
-      onChangeSendMsg(page - 1);
+  const onChangePage = useCallback(
+    (type: string, i: number) => {
+      if (type === "SEND") {
+        onChangeSendMsg(i);
+        setCurrentPage(i);
+      }
+      if (type === "RECEIVED") {
+        onChangeReceivedMsg(i);
+        setCurrentPage(i);
+      }
     },
-    [currentSendPage],
+    [currentPage],
   );
-
-  const setReceivedPage = useCallback(
-    (page: number) => {
-      setCurrentReceivedPage(page);
-      onChangeReceivedMsg(page - 1);
-    },
-    [currentReceivedPage],
-  );
-
-  const onClickDeleteMessage = useCallback(async (id: number) => {
-    await dispatch(deleteMessage(id));
-    router.refresh();
-  }, []);
-
-  const replaceUserProfile = useCallback((id: number) => {
-    router.push(`/user/${id}`);
-  }, []);
   return {
     onChangeSendMsg,
     onChangeReceivedMsg,
@@ -77,13 +64,9 @@ export default function useMessage() {
     onChangeTab,
     msgReceivedData,
     msgReceivedSize,
-    currentReceivedPage,
-    currentSendPage,
-    setSendPage,
-    setReceivedPage,
+    currentPage,
+    onChangePage,
     msgSendData,
     msgSendSize,
-    onClickDeleteMessage,
-    replaceUserProfile,
   };
 }
