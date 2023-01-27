bf8df707768e18920100b80d06735bbab6dd7f74
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-27 23:15:49 +0900

    chore: 아이콘 추가

--- src/components/profile/me/index.tsx
+++ src/components/profile/me/index.tsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 import Inform from "./inform";
 import Tab from "./tab";
-import { useBookmarkModal } from "../../../hooks/useModal";
-import BookMarkModal from "../../bookmark/bookMarkModal";
+import {
+  useFollowerBookmarkModal,
+  useFollowingBookmarkModal,
+} from "../../../hooks/useModal";
+import FollowingBookMarkModal from "../../bookmark/followingBookMark";
+import FollowerBookMarkModal from "../../bookmark/followerBookMark";
 
 const Container = styled.div`
   width: 100%;
@@ -14,12 +18,14 @@ const Container = styled.div`
 
 export default function UserProfile() {
   const [openForm, setOpenForm] = useState(false);
-  const { isBookmarkModalOpen } = useBookmarkModal();
+  const { isFollowerModalOpen } = useFollowerBookmarkModal();
+  const { isFollowingModalOpen } = useFollowingBookmarkModal();
   return (
     <Container>
       <Inform openForm={openForm} setOpenForm={setOpenForm} />
       <Tab openForm={openForm} setOpenForm={setOpenForm} />
-      {isBookmarkModalOpen && <BookMarkModal />}
+      {isFollowingModalOpen && <FollowingBookMarkModal />}
+      {isFollowerModalOpen && <FollowerBookMarkModal />}
     </Container>
   );
 }
