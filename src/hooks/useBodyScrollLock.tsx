b58c8c6ad42131bcc1c5d561e126c0c59517d015
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-27 23:13:13 +0900

    feat: 모달창 오픈 시 윈도우 스크롤 방지 hook 구현

---
+++ src/hooks/useBodyScrollLock.tsx
@@ -0,0 +1,24 @@
+import { useCallback } from "react";
+
+export default function useBodyScrollLock() {
+  let scrollPosition = 0;
+  const lockScroll = useCallback(() => {
+    // for IOS safari
+    scrollPosition = window.pageYOffset;
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollPosition}px`;
+    document.body.style.width = "100%";
+  }, []);
+
+  const openScroll = useCallback(() => {
+    // for IOS safari
+    document.body.style.removeProperty("overflow");
+    document.body.style.removeProperty("position");
+    document.body.style.removeProperty("top");
+    document.body.style.removeProperty("width");
+    window.scrollTo(0, scrollPosition);
+  }, []);
+
+  return { lockScroll, openScroll };
+}
