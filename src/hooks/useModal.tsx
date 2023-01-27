878e6b29f8d751f689947547a685032f0d4b5eaa
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-27 23:15:27 +0900

    feat: 북마크 모달 관리 hook 구현

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -2,13 +2,18 @@ import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../stores/reducers";
-import { setBookmarkModal, setMsgModal } from "../../stores/reducers/context";
+import {
+  setFollowingModal,
+  setMsgModal,
+  setFollowerModal,
+} from "../../stores/reducers/context";
 import { AppDispatch } from "../../stores/store/configureStore";
-import { loadFollowing } from "../api/bookmark";
+import { loadFollowing, loadFollower } from "../api/bookmark";
 import { sendMessage } from "../api/message";
 import { MessageProps } from "../interface/messgae";
 import { getDate, getTime } from "../util/date";
 import useInput from "./useInput";
+import useBodyScrollLock from "./useBodyScrollLock";
 
 export function useMsgModal({ item }: MessageProps) {
   const dispatch = useDispatch<AppDispatch>();
@@ -58,31 +63,80 @@ export function useMsgModal({ item }: MessageProps) {
   };
 }
 
-export function useBookmarkModal() {
+export function useFollowingBookmarkModal() {
   const dispatch = useDispatch<AppDispatch>();
-  const { isBookmarkModalOpen } = useSelector(
+  const { isFollowingModalOpen } = useSelector(
     (state: RootState) => state.context,
   );
-
-  const { lastPage } = useSelector((state: RootState) => state.bookmark);
+  const { lockScroll, openScroll } = useBodyScrollLock();
+  const { following, hasMoreFollowing, loadFollowingLoading } = useSelector(
+    (state: RootState) => state.bookmark,
+  );
 
   const openBookmarkModal = async () => {
-    dispatch(setBookmarkModal(true));
-    await dispatch(loadFollowing(0));
+    dispatch(setFollowingModal(true));
+    lockScroll();
+    if (following.length === 0) await dispatch(loadFollowing(0));
   };
   const closeBookmarkModal = () => {
-    dispatch(setBookmarkModal(false));
+    dispatch(setFollowingModal(false));
+    openScroll();
   };
 
-  const loadNext = async () => {
-    console.log("시발");
-    await dispatch(loadFollowing(lastPage));
+  const loadNext = async (num: number) => {
+    if (!loadFollowingLoading && hasMoreFollowing) {
+      await setTimeout(() => {
+        dispatch(loadFollowing(num));
+      }, 3000);
+    }
   };
 
   return {
-    isBookmarkModalOpen,
+    isFollowingModalOpen,
     openBookmarkModal,
     closeBookmarkModal,
     loadNext,
+    following,
+    hasMoreFollowing,
+    loadFollowingLoading,
+  };
+}
+
+export function useFollowerBookmarkModal() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { isFollowerModalOpen } = useSelector(
+    (state: RootState) => state.context,
+  );
+  const { lockScroll, openScroll } = useBodyScrollLock();
+  const { follower, hasMoreFollower, loadFollowerLoading } = useSelector(
+    (state: RootState) => state.bookmark,
+  );
+
+  const openBookmarkModal = async () => {
+    dispatch(setFollowerModal(true));
+    lockScroll();
+    if (follower.length === 0) await dispatch(loadFollower(0));
+  };
+  const closeBookmarkModal = () => {
+    dispatch(setFollowerModal(false));
+    openScroll();
+  };
+
+  const loadNext = async (num: number) => {
+    if (!loadFollowerLoading && hasMoreFollower) {
+      await setTimeout(() => {
+        dispatch(loadFollower(num));
+      }, 3000);
+    }
+  };
+
+  return {
+    isFollowerModalOpen,
+    openBookmarkModal,
+    closeBookmarkModal,
+    loadNext,
+    follower,
+    hasMoreFollower,
+    loadFollowerLoading,
   };
 }
