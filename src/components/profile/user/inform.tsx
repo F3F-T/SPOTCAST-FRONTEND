2146bd87428a0b91c4999236ab89e1a1c5d488db
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-01-28 18:19:08 +0900

    feat: 유저 프로필에 메시지, 팔로우 기능 추가

--- src/components/profile/user/inform.tsx
+++ src/components/profile/user/inform.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Button from "../../common/Button";
-import { GREY } from "../../../constants/colors";
-import { getUser } from "../../../util/lib";
+import { GREY, MUSTARD } from "../../../constants/colors";
+import { getUser, getMe } from "../../../util/lib";
 import { FieldProps } from "../../../interface/user";
+import Icon from "../../common/Icon";
+import useBookmark from "../../../hooks/useBookmark";
+import MsgModal from "../../message/msgModal";
+import { useMsgModal } from "../../../hooks/useModal";
 
 const Container = styled.div`
   width: 29rem;
@@ -44,12 +48,38 @@ const InformButton = styled(Button)`
   padding: 1.5rem;
   border-radius: 0.5rem;
   font-size: 1.5rem;
-  width: 100%;
+  width: 29rem;
   margin-top: 1rem;
+`;
+const OtherButton = styled.button`
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+  font-size: 1.5rem;
+  width: 14rem;
+  margin-top: 1rem;
+  border: 0.1rem solid ${GREY[500]};
+  color: ${GREY[500]};
+  background-color: white;
+  display: flex;
+  gap: 0.5rem;
+  justify-content: center;
+  &:hover {
+    color: ${GREY[600]};
+    cursor: pointer;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default function Inform() {
   const { user } = getUser();
+  const { me } = getMe();
+  const { onClickAddBookmark } = useBookmark();
+  const { isMsgModalOpen, openMsgModal } = useMsgModal(user);
+
   return (
     <Container>
       <Wrapper>
@@ -66,8 +96,18 @@ export default function Inform() {
       </Wrapper>
       <EgName>{user.egName}</EgName>
       <ProfileImg src="/images/img4.png" />
-
+      <ButtonWrapper>
+        <OtherButton onClick={() => onClickAddBookmark(me.id, user.id)}>
+          <Icon className="starEmpty" border={0.1} color={MUSTARD} />
+          Add to List
+        </OtherButton>
+        <OtherButton onClick={() => openMsgModal()}>
+          <Icon className="contact" border={0.1} />
+          Contact
+        </OtherButton>
+      </ButtonWrapper>
       <InformButton title="전체 포트폴리오 보기" buttonTheme="tertiary" />
+      {isMsgModalOpen && <MsgModal user={user} />}
     </Container>
   );
 }
