b9f50091a46f6e44436285d933c72b582b996df5
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-04 22:52:51 +0900

    feat: 구인공고 게시 기능 구현

--- src/hooks/useRecruit.ts
+++ src/hooks/useRecruit.ts
@@ -1,7 +1,27 @@
 import { useRouter } from "next/navigation";
+import { useCallback, useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import useInput from "./useInput";
+import { AppDispatch } from "../../stores/store/configureStore";
+import { getMe } from "../util/lib";
+import postRecruit from "../api/recruit";
 
 export default function useRecruit() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { me } = getMe();
+
   const router = useRouter();
+  const [title, onChangeTitle] = useInput("");
+  // 제작사 필드 아직 없음
+  const [recruitType, onChangeRecruitType] = useInput("");
+  const [recruitVolume, onChangeRecruitVolume] = useInput("");
+  const [regDate, onChangeRegDate] = useInput("");
+  const [pay, onChangePay] = useInput("");
+  const [phone, onChangePhone] = useInput("");
+  const [supportEmail, onChangeSupportEmail] = useInput("");
+  const [participationPeriod, onChangeParticipationPeriod] = useInput("");
+  const [contents, onChangeContents] = useInput("");
+  const [profitStatus, setProfitStatus] = useState("PROFITABLE");
 
   const replacePostPage = () => {
     router.push("/recruit/post");
@@ -11,8 +31,93 @@ export default function useRecruit() {
     router.push("/recruit/form");
   };
 
+  const [types, setTypes] = useState([
+    {
+      id: 0,
+      title: "수익성",
+      selected: true,
+      profitStatus: "PROFITABLE",
+    },
+    {
+      id: 1,
+      title: "비수익성(포트폴리오)",
+      selected: false,
+      profitStatus: "NON_PROFITABLE",
+    },
+  ]);
+
+  const onToggleCheck = useCallback(
+    (id: number) => {
+      setTypes(
+        types.map(type =>
+          // eslint-disable-next-line no-nested-ternary
+          type.id === id
+            ? { ...type, selected: !type.selected }
+            : type.id !== id && type.selected === true
+            ? { ...type, selected: !type.selected }
+            : type,
+        ),
+      );
+    },
+    [types],
+  );
+
+  useEffect(() => {
+    // eslint-disable-next-line array-callback-return
+    types.map(i => {
+      if (i.selected) {
+        setProfitStatus(i.profitStatus);
+      }
+    });
+    console.log(profitStatus);
+  }, [types]);
+  const onSubmitForm = () => {
+    const data = {
+      title,
+      content: contents,
+      supportEmail,
+      phone,
+      pay: +pay,
+      participationPeriod: +participationPeriod,
+      recruitVolume: +recruitVolume,
+      recruitType,
+      profitStatus,
+      boardType: "RECRUIT",
+      regDate: new Date(regDate),
+      category: {
+        // 임시 아이디
+        id: 3,
+      },
+      member: {
+        id: me.id,
+      },
+    };
+    console.log(profitStatus);
+    dispatch(postRecruit(data));
+  };
   return {
+    types,
     replacePostPage,
     replaceFormPage,
+    onToggleCheck,
+    title,
+    recruitType,
+    recruitVolume,
+    regDate,
+    pay,
+    phone,
+    supportEmail,
+    participationPeriod,
+    contents,
+    onChangeTitle,
+    onChangeRecruitType,
+    onChangeRecruitVolume,
+    onChangeRegDate,
+    onChangePay,
+    onChangePhone,
+    onChangeSupportEmail,
+    onChangeParticipationPeriod,
+    onChangeContents,
+    onSubmitForm,
   };
 }
