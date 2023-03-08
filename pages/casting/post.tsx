dabc264b9676d6feb1b62dad5f98696446edf145
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-09 00:14:48 +0900

    feat: 캐스팅 게시글 페이지 구현

---
+++ pages/casting/post.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import AppLayout from "../../styles/AppLayout";
+import wrapper from "../../stores/store/configureStore";
+import { loadMe } from "../../src/api/auth";
+import { useRedirect } from "../../src/hooks/useAuth";
+import Post from "../../src/components/casting/Post";
+
+// 추후에 다이나믹 라우팅으로 변경
+function Page() {
+  useRedirect();
+  return (
+    <AppLayout>
+      <Post />
+    </AppLayout>
+  );
+}
+
+export default Page;
+
+export const getServerSideProps = wrapper.getServerSideProps(store =>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async ({ req, res, ...etc }) => {
+    const cookie = req ? req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+
+    if (req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    await store.dispatch(loadMe());
+
+    return {
+      props: {},
+    };
+  },
+);
