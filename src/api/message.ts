9f2320c6891047e6b99a4be731b3dc83c125fa29
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-09-02 18:40:55 +0900

    refactor: 메시지 api에서 createAsyncThunk 삭제

--- src/api/message.ts
+++ src/api/message.ts
@@ -8,117 +8,51 @@ import { MsgSizeProps } from "../interface/messgae";
 axios.defaults.baseURL = API.BASE_URL;
 axios.defaults.withCredentials = true;
 
-export const preLoadMsgReceived = createAsyncThunk(
-  "message/loadMsgReceived",
-  async (data: MsgSizeProps, { rejectWithValue }) => {
-    try {
-      const { page, size } = data;
-      const response = await axios.get(
-        `${API.LOAD_MSG_RECEIVE}?page=${page}&size=${size}`,
-      );
-      return response.data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
+export const loadMsgSend = async (data: MsgSizeProps) => {
+  const { page, size } = data;
+  const response = await authorizationClient.get(
+    `${API.LOAD_MSG_SEND}?page=${page}&size=${size}`,
+  );
+  return response.data.data;
+};
+export const loadMsgReceived = async (data: MsgSizeProps) => {
+  const { page, size } = data;
+  const response = await authorizationClient.get(
+    `${API.LOAD_MSG_RECEIVE}?page=${page}&size=${size}`,
+  );
+  return response.data.data;
+};
 
-export const preLoadMsgSend = createAsyncThunk(
-  "message/loadMsgSend",
-  async (data: MsgSizeProps, { rejectWithValue }) => {
-    try {
-      const { page, size } = data;
-      const response = await axios.get(
-        `${API.LOAD_MSG_SEND}?page=${page}&size=${size}`,
-      );
-      return response.data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
+export const loadMsgUnread = async (data: MsgSizeProps) => {
+  const { page, size } = data;
+  const response = await authorizationClient.get(
+    `${API.LOAD_MSG_UNREAD}?page=${page}&size=${size}`,
+  );
+  return response.data.data;
+};
+export const sendMessage = async (data: object) => {
+  try {
+    const response = await authorizationClient.post(API.MESSAGE_SEND, data);
+    toastMsg("메시지 전송 완료", true);
+    return response.data.data;
+  } catch (error: any) {
+    toastMsg("메시지 전송 실패", false);
+    return null;
+  }
+};
 
-export const loadMsgSend = createAsyncThunk(
-  "message/loadMsgSend",
-  async (data: MsgSizeProps, { rejectWithValue }) => {
-    try {
-      const { page, size } = data;
-      const response = await authorizationClient.get(
-        `${API.LOAD_MSG_SEND}?page=${page}&size=${size}`,
-      );
-      return response.data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
+export const deleteMessage = async (id: number) => {
+  try {
+    const response = await authorizationClient.patch(`${API.MESSAGE}${id}`);
+    toastMsg("메시지 삭제 완료", true);
+    return response.data.data;
+  } catch (error: any) {
+    toastMsg("메시지 삭제 실패", false);
+    return null;
+  }
+};
 
-export const loadMsgReceived = createAsyncThunk(
-  "message/loadMsgReceived",
-  async (data: MsgSizeProps, { rejectWithValue }) => {
-    try {
-      const { page, size } = data;
-      const response = await authorizationClient.get(
-        `${API.LOAD_MSG_RECEIVE}?page=${page}&size=${size}`,
-      );
-      return response.data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const loadMsgUnread = createAsyncThunk(
-  "message/loadMsgUnread",
-  async (data: MsgSizeProps, { rejectWithValue }) => {
-    try {
-      const { page, size } = data;
-      const response = await authorizationClient.get(
-        `${API.LOAD_MSG_UNREAD}?page=${page}&size=${size}`,
-      );
-      return response.data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const sendMessage = createAsyncThunk(
-  "message/sendMessage",
-  async (data: object, { rejectWithValue }) => {
-    try {
-      const response = await authorizationClient.post(API.MESSAGE_SEND, data);
-      toastMsg("메시지 전송 완료", true);
-      return response.data.data;
-    } catch (error: any) {
-      toastMsg("메시지 전송 실패", false);
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const deleteMessage = createAsyncThunk(
-  "message/deleteMessage",
-  async (id: number, { rejectWithValue }) => {
-    try {
-      const response = await authorizationClient.patch(`${API.MESSAGE}${id}`);
-      toastMsg("메시지 삭제 완료", true);
-      return response.data.data;
-    } catch (error: any) {
-      toastMsg("메시지 삭제 실패", false);
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const readMessage = createAsyncThunk(
-  "message/readMessage",
-  async (messageId: number, { rejectWithValue }) => {
-    try {
-      await authorizationClient.patch(`${API.MESSAGE}${messageId}/read`);
-      return messageId;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
+export const readMessage = async (messageId: number) => {
+  await authorizationClient.patch(`${API.MESSAGE}${messageId}/read`);
+  return messageId;
+};
