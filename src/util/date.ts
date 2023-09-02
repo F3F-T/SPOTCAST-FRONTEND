18412bb3d795476678562a535443a2738f39a13e
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-09-02 18:45:22 +0900

    refactor: getDate util 함수 수정

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,14 +1,38 @@
-export const getDate = (itemDate: string) => {
-  if (itemDate) {
-    const newDate = itemDate?.split("T")[0].split("-");
+export const getDate = (date: string) => {
+  if (date) {
+    const newDate = date?.split("T")[0].split("-");
     return `${newDate[0]}년 ${newDate[1]}월 ${newDate[2]}일`;
   }
   return "";
 };
 
-export const getTime = (itemDate: string) => {
-  if (itemDate) {
-    const newTime = itemDate?.split("T")[1].split(":").map(Number);
+export const getMonthAndDay = (date: string) => {
+  if (date) {
+    const newDate = date?.split("T")[0].split("-");
+    return `${newDate[1]}월 ${newDate[2]}일`;
+  }
+  return "";
+};
+
+export const getRemainDay = (date: string) => {
+  const now = new Date();
+  const newDate = date?.split("T")[0].split("-").map(Number);
+  const endDate = new Date(newDate[0], newDate[1], newDate[2]);
+  const curDate = new Date(
+    now.getFullYear(),
+    now.getMonth() + 1,
+    now.getDate(),
+  );
+
+  const diff = endDate.getTime() - curDate.getTime();
+  const diffDays = diff / (1000 * 60 * 60 * 24);
+
+  return diff < 0 ? ` + ${Math.abs(diffDays)}` : ` - ${Math.abs(diffDays)}`;
+};
+
+export const getTime = (date: string) => {
+  if (date) {
+    const newTime = date?.split("T")[1].split(":").map(Number);
     if (newTime[0] === 0) {
       if (newTime[1] < 10) {
         const min = `${newTime[1].toString()}`;
