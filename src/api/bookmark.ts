d999a46ca33c342a11ff0c35aa0c135a21c32cea
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-09-10 14:38:34 +0900

    refactor: 북마크 추가, 삭제 api에서 createAsyncThunk 삭제

--- src/api/bookmark.ts
+++ src/api/bookmark.ts
@@ -1,4 +1,3 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
 import { authorizationClient } from ".";
 import toastMsg from "../components/common/Toast";
 import API from "./config";
@@ -17,31 +16,24 @@ export const loadFollowing = async (page: number) => {
   return response.data.data;
 };
 
-export const addBookmark = createAsyncThunk(
-  "bookmark/addBookmark",
-  async (data: object, { rejectWithValue }) => {
-    try {
-      const response = await authorizationClient.post(API.BOOKMARK, data);
-      toastMsg("팔로우 완료", true);
-      return response.data.data;
-    } catch (error: any) {
-      toastMsg("팔로우 실패. 다시 시도해주세요", false);
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-export const deleteBookmark = createAsyncThunk(
-  "bookmark/deleteBookmark",
-  async (data: object, { rejectWithValue }) => {
-    try {
-      const response = await authorizationClient.delete(API.BOOKMARK, {
-        data,
-      });
-      toastMsg("팔로우 취소 완료", true);
-      return response.data.data;
-    } catch (error: any) {
-      toastMsg("팔로우 취소 실패. 다시 시도해주세요", false);
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
+export const addBookmark = async (data: object) => {
+  try {
+    const response = await authorizationClient.post(API.BOOKMARK, data);
+    toastMsg("팔로우 완료", true);
+    return response.data.data;
+  } catch (error: any) {
+    toastMsg("팔로우 실패. 다시 시도해주세요", false);
+  }
+};
+
+export const deleteBookmark = async (data: object) => {
+  try {
+    const response = await authorizationClient.delete(API.BOOKMARK, {
+      data,
+    });
+    toastMsg("팔로우 취소 완료", true);
+    return response.data.data;
+  } catch (error: any) {
+    toastMsg("팔로우 취소 실패. 다시 시도해주세요", false);
+  }
+};
