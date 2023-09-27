6be06249a33fe2c3e133dc4668bbaa85317f7004
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-09-27 18:51:26 +0900

    fix: 팔로잉, 팔로워 삭제/수정 시 mutate 발생시키도록 수정

--- src/hooks/useBookmark.ts
+++ src/hooks/useBookmark.ts
@@ -1,11 +1,15 @@
+import { useSWRConfig } from "swr";
 import { addBookmark, deleteBookmark } from "../api/bookmark";
+import swrKeys from "../constants/swrKeys";
 
 export default function useBookmark() {
+  const { mutate } = useSWRConfig();
   const onClickDeleteBookmark = async (
     followerId: number,
     followingId: number,
   ) => {
     await deleteBookmark({ followerId, followingId });
+    mutate(swrKeys.loadMeKey);
   };
 
   const onClickAddBookmark = async (
@@ -13,6 +17,7 @@ export default function useBookmark() {
     followingId: number,
   ) => {
     await addBookmark({ followerId, followingId });
+    mutate(swrKeys.loadMeKey);
   };
   return {
     onClickDeleteBookmark,
