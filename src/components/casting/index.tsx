9516a154fd73e44a604e4267c2feecbfc4246d2c
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-10-02 21:24:42 +0900

    feat: 캐스팅/오디션 페이징 추가

--- src/components/casting/index.tsx
+++ src/components/casting/index.tsx
@@ -1,29 +1,38 @@
 import React from "react";
-import useSWR from "swr";
+import Pagination from "react-js-pagination";
 import CastingCard from "./CastingCard";
-import { CardGrid } from "./index.styles";
+import { CardGrid, Paging, Container } from "./index.styles";
 import CastingNav from "./Nav";
-import swrKeys from "../../constants/swrKeys";
-import { loadBoard } from "../../api/board";
-import BOARD_TYPE from "../../constants/boardType";
 import { CastingProps } from "../../interface/borad";
+import Icon from "../common/Icon";
+import useCasting from "../../hooks/useCasting";
 
 export default function Casting() {
-  const { data } = useSWR(swrKeys.loadCastingKey, () =>
-    loadBoard(BOARD_TYPE.CASTING_AUDITION, 0, null, 0, 4),
-  );
-  console.log(data);
-
+  const { data, currentPage, onChangePage } = useCasting();
   if (!data) return null;
 
   return (
     <>
       <CastingNav />
-      <CardGrid>
-        {data.data.content.map((info: CastingProps) => (
-          <CastingCard info={info} />
-        ))}
-      </CardGrid>
+      <Container>
+        <CardGrid>
+          {data.data.content.map((info: CastingProps) => (
+            <CastingCard info={info} />
+          ))}
+        </CardGrid>
+        <Paging>
+          <Pagination
+            activePage={currentPage}
+            itemsCountPerPage={12}
+            totalItemsCount={data.data.totalElements}
+            pageRangeDisplayed={5}
+            prevPageText={<Icon className="arrowLeft" size="1.2rem" />}
+            nextPageText={<Icon className="arrowRight" size="1.2rem" />}
+            hideFirstLastPages
+            onChange={onChangePage}
+          />
+        </Paging>
+      </Container>
     </>
   );
 }
