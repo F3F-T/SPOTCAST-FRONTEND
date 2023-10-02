8369a04b53a686a72e3637f9560df96c7c5eb0fb
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-10-02 21:22:26 +0900

    feat: CATEGORY_ID 상수 추가

--- src/constants/boardType.ts
+++ src/constants/boardType.ts
@@ -3,4 +3,16 @@ const BOARD_TYPE = {
   CASTING_AUDITION: "CASTING_AUDITION",
 } as const;
 
-export default BOARD_TYPE;
+const CATEGORY_ID = {
+  실용음악: 3,
+  클래식: 4,
+  영화: 5,
+  드라마: 6,
+  연극: 7,
+  "방송/예능": 8,
+  모델: 9,
+  광고: 10,
+  기타: 11,
+};
+
+export { BOARD_TYPE, CATEGORY_ID };
