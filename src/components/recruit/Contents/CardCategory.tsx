0659604920d356b01713b2ac6fce9cd3a64384a5
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-10-06 20:04:35 +0900

    feat: 서버 데이터로 구인공고 리스트 데이터 페칭

--- src/components/recruit/Contents/CardCategory.tsx
+++ src/components/recruit/Contents/CardCategory.tsx
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 import React from "react";
 import { GREY } from "../../../constants/colors";
 import Badge from "../../common/Badge";
+import { PROFITABLE_STATUS } from "../../../constants/boardType";
+import { IBoard } from "../../../interface/borad";
 
 const Container = styled.div`
   width: 27rem;
@@ -26,12 +28,19 @@ const StyledBadge = styled(Badge)`
   font-size: 1.3rem;
   margin-bottom: 0.5rem;
 `;
-export default function CardCategory() {
-  const Categories = ["제작", "작품 제목", "분야"];
-  const Contents = ["강준우", "이별 이야기(가제)", "촬영"];
+export default function CardCategory({ info }: { info: IBoard }) {
+  const Categories = ["제작", "분야"];
+  const Contents = [info.production, info.recruitType];
   return (
     <Container>
-      <StyledBadge label="수익성" variant="indigo" />
+      <StyledBadge
+        label={
+          info.profitStatus === PROFITABLE_STATUS.PROFITABLE
+            ? "수익성"
+            : "비수익성(포트폴리오)"
+        }
+        variant="indigo"
+      />
       {Categories.map((ele, idx) => {
         return (
           <Wrapper>
